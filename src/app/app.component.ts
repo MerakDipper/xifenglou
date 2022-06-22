21d29d3302a1428811bb17908777f9bbac9ccc96
Xuan Zhang <meraktx@gmail.com>
2022-06-22 18:39:46 -0400

    saved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -89,6 +89,7 @@ export class AppComponent {
         break;
       case 'SEX':
         tempEvent.isSex = true;
+        console.log(jueji.xingzhi);
         if(jueji.xingzhi < 0.8) {
         tempEvent.eventText += '对方的兴致似乎不太高，再闲聊一会儿吧～\n';
 	}else{
